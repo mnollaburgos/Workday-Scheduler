88579cad892c53df7c2616c694bbd34dd5076d0b
Miguel Nolla <nolla.carlos@gmail.com>
2021-01-23 15:09:00 -0500

    New Revisions for review.

--- Asstes/index.js
+++ Asstes/index.js
@@ -1,16 +1,11 @@
-//let timesArr= ["9AM","10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]; 
-//for (let i=1; i<timesArr.length; i++){
- //   let newTimeBlockEL= $("#time").clone();
-   // newTimeBlockEL.attr("id", timesArr[i]); 
-    //newTimeBlockEL.children(".row").attr("style", "white-space: pre-Wrap"); 
-    //newTimeBlockEL.children(".row").children(".hour").text(timesArr[i]); 
-    //newTimeBlockEL.appendTo(".container"); 
-//}
 
-
-$(".saveBtn").on("click", function() {
-var input = $("#textInput").val().trim();
-localStorage.setItem('value', input);
-localStorage.getItem(input);
-}
-)
+$(document).ready(function(){
+    $(".saveBtn").on("click", function(e) {
+        var input = $("#textInput").val().trim();
+        localStorage.setItem('value', input);
+        localStorage.getItem('value');
+        e.preventDefault();
+        }
+        )
+        
+})

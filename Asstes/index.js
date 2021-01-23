2e6595c2523376a087fc47037ef7d7f208be7ad5
Miguel Nolla <nolla.carlos@gmail.com>
2021-01-23 15:18:02 -0500

    Revisions

--- Asstes/index.js
+++ Asstes/index.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
         localStorage.setItem('value', input);
         localStorage.getItem('value');
         e.preventDefault();
-        }
-        )
+        })
         
 })
+
+window.setInterval(function(){
+    now = new Date;
+  c = now.getMinutes();
+  if (c < 60 ) {
+  $('.container').css('background', 'red');
+  }
+  }, 1000);
+  
